ddc64e6691c4e60a7f835b3acc687f04b985ab28
A04987 <gasidit0anu@gmail.com>
2025-10-21 20:55:44 +0700

    Fully charged

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,18 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './features/Navbar';
 import Container from './features/Container';
 import Home from './features/Home';
-
+import AddForm from './features/Product/AddForm';
+import UpdateForm from './features/Product/UpdateForm';
+import { fetchProducts } from './features/Product/actions';
+import GlobalStyle from './features/GlobalStyle';
 
 function App() {
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function getProducts() {
+        const response = await axios.get(
+          'https://68e9fffbf1eeb3f856e5bb1d.mockapi.io/React/products'
+        );
+        dispatch(fetchProducts(response.data));
+      }
+
+    getProducts();
+  }, []);
+
   return (
-    <div>
+    <>
+      <GlobalStyle />
       <Navbar />
       <Container>
-      <Home />
+        {products.length > 0 ? (
+          <Routes>
+            <Route path="/create-product" element={<AddForm />} />
+            <Route path="/update-product/:id" element={<UpdateForm />} />
+            <Route path="/" element={<Home products={products} />} />
+          </Routes>
+        ) : (
+          <div>Loading products...</div>
+        )}
       </Container>
-    </div>
+    </>
   );
 }
 
--- ecommerce-app/src/features/Home.js
+++ ecommerce-app/src/features/Home.js
@@ -1,19 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import data from '../app/data';
 import Product from './Product';
 
-export default function Home() {
-  const [products, setProducts] = useState(data);
-
+function Home({ className, products }) {
   return (
-    <div>
+    <div className={className}>
       <h1>New Products</h1>
-      <ul className="Home__products">
-        {products.map((product) => (
-          <Product key={product.id} item={product} />
-        ))}
-      </ul>
+      {products.length > 0 ? (
+        <ul className="Home__products">
+          {products.map((product) => (
+            <Product key={product.id} item={product} />
+          ))}
+        </ul>
+      ) : (
+        <div>Loading products....</div>
+      )}
     </div>
   );
 }
+
+Home.propTypes = {
+  className: PropTypes.string.isRequired,
+  products: PropTypes.array.isRequired
+};
+
+export default styled(Home)`
+  .Home__products {
+    display: flex;
+    flex-wrap: wrap;
+
+    list-style-type: none;
+    padding: 0;
+    margin: 0 -12px;
+  }
+`;
