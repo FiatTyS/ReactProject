44cb7c6eb633797c63696ff2816ed375a476e002
A04987 <gasidit0anu@gmail.com>
2025-10-21 21:46:44 +0700

    4

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TodoListPage from "./pages/TodoListPage";
+import EditTodoPage from "./pages/EditTodoPage";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TodoListPage />} />
+        <Route path="/edit/:id" element={<EditTodoPage />} />
+      </Routes>
+    </Router>
+  );
+}
